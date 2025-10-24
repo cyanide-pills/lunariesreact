f9ec0913dea65092bb891a502900a66435fc9ebb
sofía <so.mayorga@duocuc.cl>
2025-10-23 22:11:55 -0300

    Css fix

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -5,10 +5,18 @@ function Welcome() {
   return (
     <div id= 'Welcome'>
         <div className='Showcase-Display'>
+          
+            <div className='Display'>
 
-            <div className='Display-Text'>
-                <h1>Bienvenido a Lunaries</h1>
-                <p>Tu tienda en línea de confianza para productos de alta calidad.</p>
+              <div className='Display-Text'>
+                  <h1>Bienvenido!</h1>
+                  <p>Tu tienda en línea de confianza para productos de alta calidad.</p>
+              </div>
+
+              <div className='Display-Image'>
+                  <img src='src/assets/images/paintings/cv1.png' />
+              </div>
+
             </div>
 
         </div>
